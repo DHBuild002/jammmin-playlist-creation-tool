595ad091998c2b1682ad9ff11ab8871407f069bc
Dan Haddock <dhbuild.002@gmail.com>
2024-09-27 21:19:12 +0100

    FIX: Part 1 of implementing Login logic from ChatGPT example

---
+++ src/components/Spotify.js
@@ -0,0 +1,16 @@
+const authUrl = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}&response_type=token&scope=user-read-private`;
+
+export const loginToSpotify = () => {
+  window.location.href = authUrl;
+};
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((acc, item) => {
+      let parts = item.split("=");
+      acc[parts[0]] = decodeURIComponent(parts[1]);
+      return acc;
+    }, {}).access_token;
+};
