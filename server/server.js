34ab96b06894df3fc3ec348339d7c05ba9261e8e
Dan Haddock <dhbuild.002@gmail.com>
2024-09-28 10:41:55 +0100

    Port: server folder with env variables and search logic

---
+++ server/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config({ path: "./server/variables.env" });
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.get("/search", async (req, res) => {
+  const query = req.query.query;
+  try {
+    const token = await getAccessToken();
+
+    const search_url = "https://api.spotify.com/v1/search";
+    const response = await axios.get(search_url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: query,
+        type: "track",
+        limit: 3,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error in /search endpoint:", error);
+    res.status(500).send("Error performing search");
+  }
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
