dadd42564369366489e1ae54b33f912e576ffa4f
Dan Haddock <dhbuild.002@gmail.com>
2024-10-11 18:38:14 +0100

    FIX: Apply styling fix to the enter name field for Playlist Creator

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -57,35 +57,35 @@ const Playlist = ({
 
   return (
     <>
-      <div className="playlist">
-        <div className="flex flex-row m-auto justify-center text-2xl mt-4">
+      <div className="">
+        <div className="flex flex-row justify-center items-center text-2xl mt-4">
           {/* {/* <h2
             className="cursor-pointer mr-2 text-purple-950 border-b pb-1 border-y-white w-1/3"
             onClick={handleEditClick}
           > 
             isInputVisible ? 'Enter Text' : savedPlaylistName
           </h2> */}
+          {isInputVisible ? (
+            <input
+              className="bg-white rounded-lg text-sm p-3 cursor-pointer border-gray-400"
+              value={playlistName}
+              onChange={handleNameChange}
+              onFocus={handleFocus}
+              placeholder="Enter a name for your playlist..."
+            />
+          ) : (
+            <h2
+              className="cursor-pointer text-purple-950 border-b pb-1 border-b-white w-1/2 mr-3 mt-5"
+              onClick={handleEditClick}
+            >
+              {playlistName}
+            </h2>
+          )}
           <EditIcon
-            className="edit-icon mt-1 border-purple-500 border-solid border-2 cursor-pointer text-purple-500 p-x bg-white rounded-lg text-whtie"
+            className="edit-icon border-purple-500 border-solid border-2 cursor-pointer text-purple-500 p-x bg-white rounded-lg text-whtie"
             onClick={handleEditClick}
           />
         </div>
-        {isInputVisible ? (
-          <input
-            className="bg-white rounded-lg text-sm p-3 cursor-pointer border-gray-400"
-            value={playlistName}
-            onChange={handleNameChange}
-            onFocus={handleFocus}
-            placeholder="Enter a name for your playlist..."
-          />
-        ) : (
-          <h2
-            className="cursor-pointer mr-2 text-purple-950 border-b pb-1 border-b-white w-1/3"
-            onClick={handleEditClick}
-          >
-            {playlistName}
-          </h2>
-        )}
 
         {isInputVisible && (
           <div>
