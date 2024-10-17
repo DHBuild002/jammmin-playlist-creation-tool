39fc756c982ed0bee5936cfdf722a3643dceade7
Dan Haddock <dhbuild.002@gmail.com>
2024-10-17 19:49:49 +0100

    FIX: Improved state handling on initial load - User will have to login on first load of the page - Clear token on unload event

--- src/App.js
+++ src/App.js
@@ -87,6 +87,9 @@ function Callback({ setToken }) {
 
 function App() {
   const [token, setToken] = useState();
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
 
   const saveToken = (token) => {
     localStorage.setItem("spotify_access_token", token);
